764d078e2985d4bbf0f3dd807ee84fb71016dae5
Matt <marosenthal18@gmail.com>
2022-05-20 16:07:01 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./App.css";
+
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US")
+}
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2022, 6, 0),
+        end: new Date(2022, 6, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2022, 6, 7),
+        end: new Date(2022, 6, 10),
+    },
+    {
+        title: "Conference",
+        start: new Date(2022, 6, 20),
+        end: new Date(2022, 6, 23),
+    },
+];
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" style={{ height: 700, margin: "50px" }} />
+    </div>
+  );
+}
+
+export default App;
